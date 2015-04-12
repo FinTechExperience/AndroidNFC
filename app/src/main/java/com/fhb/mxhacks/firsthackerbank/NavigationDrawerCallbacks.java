70cc8554fa34438ecd2a6d2568eeffe7a0999b0b
Ricardo <ricktter@gmail.com>
2015-04-12 09:56:42 -0500

    first commit

---
+++ app/src/main/java/com/fhb/mxhacks/firsthackerbank/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package com.fhb.mxhacks.firsthackerbank;
+
+/**
+ * Created by Ricardo on 12/04/2015.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
